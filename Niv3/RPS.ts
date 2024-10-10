780903cae38489e4ceaa07e31a44b4100a321418
Nathan Liove <liove.nathan@orange.fr>
2024-10-10 14:26:07 +0200

    Ajout de rps(niv3)

---
+++ Niv3/RPS.ts
@@ -0,0 +1,47 @@
+import * as readline from 'readline';
+var a=2;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+var randomNumber = Math.floor(Math.random() * 3 + 1);
+
+function rps(randomNumber: number): void {
+    rl.question("Rock, Paper, ou Scissors? ", function(answer) {
+        
+        if (answer == "Paper") {
+            if (randomNumber == 1) {
+                console.log("Match Nul");
+            } else if (randomNumber == 2) {
+                console.log("Perdu");
+            } else if (randomNumber == 3) {
+                console.log("Gagné");
+                rl.close();
+                return;
+            }
+        } else if (answer == "Rock") {
+            if (randomNumber == 1) {
+                console.log("Perdu");
+            } else if (randomNumber == 2) {
+                console.log("Gagné");
+                rl.close();
+                return;
+            } else if (randomNumber == 3) {
+                console.log("Match Nul");
+            }
+        } else if (answer == "Scissors") {
+            if (randomNumber == 1) {
+                console.log("Gagné");
+                rl.close();
+                return;
+            } else if (randomNumber == 2) {
+                console.log("Match Nul");
+            } else if (randomNumber == 3) {
+                console.log("Perdu");
+            }
+        }
+        randomNumber = Math.floor(Math.random() * 3 + 1);
+        rps(randomNumber);
+    });
+}
+rps(randomNumber);
